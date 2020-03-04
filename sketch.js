34a6f3849cfec54957effdc4460b36cbccb0f75a
Leon Rogge <37003958+E00S@users.noreply.github.com>
2020-03-03 20:01:25 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,77 +1,32 @@
 const AIO_USERNAME = "E00S"
-const AIO_KEY = "aio_Turw62EQw6UOfy9uLqocPoD6K2CD"
+const AIO_KEY = "aio_AQvo77QqcZ6gOmsvCUJWdFZxdAP8"
 
 function setup() {
-    let canvas = createCanvas(600, 600)
+    let canvas = createCanvas(windowWidth/2, windowHeight/3)
+    canvas.position(windowWidth/4, (windowHeight/1.5))
     noLoop()
 }
 
 async function draw() { // note "async" keyword
 
-    // fetch our data
-    let data = await fetchData("sensor-test")      // note the "await" keyword
-    // print(data)
-
-    // // re-sort the array by time
-    // data.sort((a, b) => (a.created_at > b.created_at) ? 1 : -1)
-
-    // // make a new array with just the sensor values
-    // // divide by the max value to "normalize" them to the range 0-1
-    // let values = []
-    // for (let datum of data) {
-    //     values.push(datum.value / 4095)
-    // }
-
-    // // make a new array with just the timestamp
-    // // this one is trickier to normalize so we'll do it separately
-    // let times = []
-    // for (let datum of data) {
-    //     // convert the string into a numerical timestamp
-    //     let time = Date.parse(datum.created_at) / 1000
-    //     times.push(time)
-    // }
-
-    // // normalize the times to between 0 and 1
-    // let start_time = min(times)
-    // let stop_time = max(times)
-    // for (let i=0; i<times.length; i++) {
-    //     let time = times[i]
-    //     times[i] = (time - start_time) / (stop_time - start_time)
-    // }
-
-    // now we can draw
-
     background(0)
 
-    // make colors
-    // in this case, we want a line every pixel, and to interpolate between values
-    // colorMode(HSB) // https://p5js.org/reference/#/p5/colorMode
-    // for (let i=1; i<values.length; i++) {
-    //     let x1 = int(times[i-1] * width)
-    //     let x2 = int(times[i] * width)
-    //     print(x1, x2)
-    //     let c1 = color(55, values[i-1] * 255, 255)
-    //     let c2 = color(55, values[i] * 255, 255)
-    //     for (let x=x1; x<=x2; x++) {
-    //         let interpolation = (x-x1) / (x2-x1)
-    //         print(x, interpolation)
-    //         let c = lerpColor(c1, c2, interpolation)
-    //         stroke(c)
-    //         line(x, 0, x, height)
-    //     }
-    // }
+    // fetch our data
+    let data = await fetchData("sensor-test")      // note the "await" keyword
+    print(data)
 
-    // this one is just a breakpoint line, similar to the adafruit feed page
-    // note that to get the y axis right, we have to flip it by subtracting from 1
-    // stroke(0)
-    // strokeWeight(2)
-    // for (let i=1; i<values.length; i++) {   // starting at 1, not 0
-    //     let x1 = times[i-1] * width
-    //     let y1 = (1 - values[i-1]) * height
-    //     let x2 = times[i] * width
-    //     let y2 = (1 - values[i]) * height
-    //     line(x1, y1, x2, y2)
-    // }
+    let steps = 0
+    for (let datum of data) {
+        steps += int(datum.value)
+        print(steps)
+    }
+    textFont('monospace')
+    textSize(width/6);
+    fill(255);
+    textAlign(CENTER,CENTER);
+    text(steps,windowWidth/4,windowHeight/12);
+    stroke(255);
+    line(0, windowHeight/6, windowWidth/4, windowHeight/6);
 
 }
 
